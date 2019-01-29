47a09f0e3f31de9725c84938deaa0de241cacc2c
AndreyRafalsky <rafalsky.work@gmail.com>
2019-01-29 16:12:40 +0200

    Initial commit

---
+++ intentutils/src/main/java/com/rafalsky/intentutils/IntentUtils.java
@@ -0,0 +1,12 @@
+package com.rafalsky.intentutils;
+
+import android.content.Intent;
+
+public class IntentUtils {
+    public static Intent getSendIntent(String message, String subject){
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        return intent;
+    }
+}
