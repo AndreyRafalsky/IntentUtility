e33b921bdd9648d2d9dabe1049d16a4d2869e01c
AndreyRafalsky <rafalsky.work@gmail.com>
2019-01-29 16:19:57 +0200

    add comment

--- intentutils/src/main/java/com/rafalsky/intentutils/IntentUtils.java
+++ intentutils/src/main/java/com/rafalsky/intentutils/IntentUtils.java
@@ -3,6 +3,7 @@ package com.rafalsky.intentutils;
 import android.content.Intent;
 
 public class IntentUtils {
+    //first intent method
     public static Intent getSendIntent(String message, String subject){
         Intent intent = new Intent(Intent.ACTION_SEND);
         intent.putExtra(Intent.EXTRA_TEXT, message);
